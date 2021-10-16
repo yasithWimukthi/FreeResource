95d051d82f8c00ff660c6ca25c70c2975c9f7e8a
yasithWimukthi <it19966922@my.sliit.lk>
2021-10-16 21:55:49 +0530

    create CourseController

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AwardingBodyController extends Controller
+class CourseController extends Controller
 {
     /**
      * Display a listing of the resource.
